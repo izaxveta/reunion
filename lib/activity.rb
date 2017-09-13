872b6ab4249354510abdf96d9546dedc9f91cb41
Iza Jimenez <izavetha@gmail.com>
2017-09-13 14:01:51 -0600

    Created add_participant method

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,18 +1,24 @@
 class Activity
   attr_reader :activity,
               :names,
-              :bills,
-              :total_tab
+              :bills
+  attr_accessor :total_tab
 
   def initialize(activity, participants)
     @activity = activity
     @names = participants[:name]
     @bills = participants[:cost]
-    @total_tab = participants[:cost].sum
+    @total_tab = bills.sum
   end
 
   def split_bill
     total_tab/names.count
+  end
+
+  def add_participant(name, bill)
+    names << name
+    bills << bill
+    @total_tab += bill
   end
 
   def bill_difference(name)
