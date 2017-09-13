36bb181e201b4eb3c01f5b79c719341052801377
Iza Jimenez <izavetha@gmail.com>
2017-09-13 10:00:41 -0600

    Activity class completed

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,24 @@
 class Activity
+  attr_reader :activity,
+              :names,
+              :bills,
+              :total_tab
+
+  def initialize(activity, participants)
+    @activity = activity
+    @names = participants[:name]
+    @bills = participants[:cost]
+    @total_tab = participants[:cost].sum
+  end
+
+  def split_bill
+    total_tab/names.count
+  end
+
+  def bill_difference(name)
+    index = names.index(name)
+    bill = bills[index]
+    bill - split_bill
+  end
+
 end
