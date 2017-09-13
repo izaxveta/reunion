e7de5fa81443f277151aaf6ab4054fe3153d3646
Iza Jimenez <izavetha@gmail.com>
2017-09-13 10:02:09 -0600

    Reunion class completed to Iteration 4

--- lib/reunion.rb
+++ lib/reunion.rb
@@ -1,2 +1,20 @@
 class Reunion
+  attr_reader :location,
+              :activity
+  def initialize(location, activity)
+    @location = location
+    @activity = [activity]
+  end
+
+  def add_activity(new_activity)
+    activity << new_activity
+  end
+
+  def total_cost
+    total = activity.map do |plan|
+      plan.total_tab
+    end
+    total.sum
+  end
+  
 end
