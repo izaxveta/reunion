f9c8063a7a257b1a2794c4cfb046eeb299ffad63
Iza Jimenez <izavetha@gmail.com>
2017-09-13 10:01:00 -0600

    Activity class test complete

--- test/activity_test.rb
+++ test/activity_test.rb
@@ -4,4 +4,43 @@ require 'minitest/pride'
 require './lib/activity'
 
 class ActivityTest < Minitest::Test
+  attr_reader :playdate
+
+  def setup
+    @playdate = Activity.new("Brunch", {name: ["Cereza", "Sandra"], cost: [50, 40]})
+  end
+
+  def test_it_exists
+    assert_instance_of Activity, playdate
+  end
+
+  def test_it_has_an_activity
+    assert_equal "Brunch", playdate.activity
+  end
+
+  def test_it_has_participants
+    assert_equal ["Cereza", "Sandra"], playdate.names
+  end
+
+  def test_each_has_their_own_bill
+    assert_equal 50, playdate.bills[0]
+  end
+
+  def test_can_access_total_bill
+    assert_equal 90, playdate.total_tab
+  end
+
+  def test_can_split_bill_evenly_between_participants
+    assert_equal 45, playdate.split_bill
+  end
+
+  def test_participant_owes_after_bill_difference
+    assert_equal 5, playdate.bill_difference("Cereza")
+  end
+
+  def test_participant_is_owed_after_bill_difference
+    assert_equal -5, playdate.bill_difference("Sandra")
+  end
+
+
 end
